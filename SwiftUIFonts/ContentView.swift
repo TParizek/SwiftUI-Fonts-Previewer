130bddadda54ee77de536ba634ef048f83fced23
Tomas Parizek <parizek@damidev.com>
2020-02-22 09:41:02 +0100

    Initial Commit

---
+++ SwiftUIFonts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIFonts
+//
+//  Created by Tomas Parizek on 2/22/20.
+//  Copyright © 2020 Tomas Parizek. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
